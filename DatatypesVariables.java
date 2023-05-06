c8b1d75455c4fdd2e79c0a1aa20d894636aececc
wahidursqa <wahidursqa@gmail.com>
2023-05-06 20:16:29 +0600

    Started Basic Java

---
+++ DatatypesVariables.java
@@ -0,0 +1,29 @@
+
+public class DatatypesVariables {
+
+
+		public static void main(String[] args) 
+		{
+			
+			
+			int a=5,b=15,c=25;
+			int sum=a+b+c;
+			
+			System.out.println(a);
+			System.out.println(b);
+			System.out.println(c);
+			
+			System.out.println(a+"  "+b+"   "+c);  //5  15   25
+			
+			
+			System.out.println("Value of a is:"+a);
+			System.out.println("Value of b is:"+b);
+			System.out.println("Value of c is:"+c);
+			
+			System.out.println("Total value of a,b,c is:" +sum); 
+			
+		
+
+	}
+
+}
---
+++ DatatypesVariables2.java
@@ -0,0 +1,52 @@
+
+
+public class DatatypesVariables2 {
+
+	public static void main(String[] args) {
+		
+		
+				int a=25, b=45;
+				
+				System.out.println("The value of a is:"+a);
+				System.out.println("The value of b is:"+b);
+				
+				long x=1234567889;
+				System.out.println("The value of x is:"+x);
+				
+				byte by=100;
+				System.out.println("The value of by is:"+by);
+				
+				short sh=32765;
+				System.out.println("The value of sh is: "+sh);
+				
+				//decimal numbers - float, double
+				
+				float item_price=26.605F;
+				System.out.println(item_price);
+				
+				double d=1234.7874564484845;
+				System.out.println(d);
+				
+				 
+				double dub=450;   
+				System.out.println(dub);
+				
+				
+				char grad='A';
+				System.out.println("The grad of the stdent is:"+grad);
+				
+				boolean bo=true;
+				System.out.println(bo);
+				
+				
+				String city_name="Dhaka";
+				System.out.println("The city of the person is:"+city_name);
+				
+				String str="A";  
+				System.out.println(str);
+				
+				String sr="true";
+
+	}
+
+}
