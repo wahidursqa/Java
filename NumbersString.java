fec86861bc056c173a7b72f480079a84f726f327
wahidursqa <wahidursqa@gmail.com>
2023-05-06 20:51:07 +0600

    class add

---
+++ NumbersString.java
@@ -0,0 +1,23 @@
+
+public class NumbersString {
+
+	public static void main(NumbersString[] args) {
+		
+		
+				//int a=123;
+				//String s="123";  //valid
+				
+				//int s="123";  // invalid
+				
+			
+				
+				System.out.println("welcome"+100);
+				System.out.println("welcome"+"100");
+				
+				System.out.println("10"+"20"); //1020
+				
+				System.out.println(10+20); //30
+
+	}
+
+}
