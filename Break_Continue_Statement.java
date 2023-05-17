d73e35c34a770dedd940216cce819a3c0ab0f6d1
wahidursqa <wahidursqa@gmail.com>
2023-05-17 21:33:52 +0600

    Break Continue Statement

---
+++ Break_Continue_Statement.java
@@ -0,0 +1,24 @@
+public class Break_Continue_Statement {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++) 
+		{
+			if(i==8)
+			{
+				break;
+			}
+			System.out.println(i);  
+		}
+
+	/*	for(int i=1;i<=10;i++)  
+		{
+			if(i==4 || i==7)
+			{
+				continue;
+			}
+			System.out.println(i);  
+		}
+		*/
+	}
+
+}
